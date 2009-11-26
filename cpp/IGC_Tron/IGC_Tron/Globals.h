bb9ff934df6296c0018558cab1689ae63e4c364c
fenrhil <fenrhil@de5929ad-f5d8-47c6-8969-ac6c484ef978>
2009-11-26 15:35:58 +0000

    Player + Vehicle

--- cpp/IGC_Tron/IGC_Tron/Globals.h
+++ cpp/IGC_Tron/IGC_Tron/Globals.h
@@ -12,20 +12,84 @@
 namespace KeyCodes
 {
 #ifdef _WIN32
-	const int ENTER = 13;
-	const int ESCAPE = 27;
-	const int UP = 38;
-	const int LEFT = 37;
-	const int RIGHT = 39;
-	const int DOWN = 40;
+const int ENTER = 13;
+const int ESCAPE = 27;
+const int UP = 38;
+const int LEFT = 37;
+const int RIGHT = 39;
+const int DOWN = 40;
 #else
-	const int ENTER = 36;
-	const int ESCAPE = 9;
-	const int UP = 111;
-	const int LEFT = 113;
-	const int RIGHT = 114;
-	const int DOWN = 116;
+const int ENTER = 36;
+const int ESCAPE = 9;
+const int UP = 111;
+const int LEFT = 113;
+const int RIGHT = 114;
+const int DOWN = 116;
 #endif
+}
+
+namespace Utils
+{
+struct CartesianVector
+{
+	double x, y, z;
+
+	CartesianVector& operator +=(const CartesianVector& c)
+	{
+		x += c.x;
+		y += c.y;
+		z += c.z;
+		return *this;
+	}
+
+	CartesianVector& operator -=(const CartesianVector& c)
+	{
+		x -= c.x;
+		y -= c.y;
+		z -= c.z;
+		return *this;
+	}
+
+	CartesianVector& operator *=(double c)
+	{
+		x *= c;
+		y *= c;
+		z *= c;
+		return *this;
+	}
+
+	CartesianVector& operator /=(double c)
+	{
+		x /= c;
+		y /= c;
+		z /= c;
+		return *this;
+	}
+
+	CartesianVector(double xi, double yi, double zi) :
+		x(xi), y(yi), z(zi)
+	{
+	}
+};
+
+CartesianVector operator +(const CartesianVector& c1, const CartesianVector& c2)
+{
+	CartesianVector res(c1);
+	res += c2;
+	return res;
+}
+
+CartesianVector operator *(double d, const CartesianVector& c)
+{
+	CartesianVector res(c);
+	res *= d;
+	return res;
+}
+
+inline CartesianVector operator *(const CartesianVector& c, double d)
+{
+	return c * d;
+}
 }
 
 #endif // __GLOBALS_H__
