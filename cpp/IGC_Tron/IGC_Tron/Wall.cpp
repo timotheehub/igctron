24c5ce82b121fc120e9b65132ac59337e60f99ba
ttjs <ttjs@de5929ad-f5d8-47c6-8969-ac6c484ef978>
2009-12-10 16:40:32 +0000

    Des murs

--- cpp/IGC_Tron/IGC_Tron/Wall.cpp
+++ cpp/IGC_Tron/IGC_Tron/Wall.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include "Wall.h"
 #include "Globals.h"
+#include "Displayer.h"
 using namespace std;
 using namespace Utils;
 
@@ -28,51 +29,35 @@ void Wall::NewVertex()
 
 void Wall::Draw() const
 {
-	/*
 	Displayer *aDisplayer = Displayer::GetInstance ();
 	IGC::Renderer *renderer = aDisplayer->GetRenderer ( );
 	IGC::Factory *factory = aDisplayer->GetFactory ( );
 
 	IGC::Model* model;
-	switch ( playerNumber )
-	{
-		case 0: model = factory->acquire( (IGC::Model*)NULL, "model_ship1" );
-			break;
-		case 1: model = factory->acquire( (IGC::Model*)NULL, "model_ship2" );
-			break;
-		case 2: model = factory->acquire( (IGC::Model*)NULL, "model_ship3" );
-			break;
-		case 3: model = factory->acquire( (IGC::Model*)NULL, "model_ship4" );
-			break;
-		default:
-			model = factory->acquire( (IGC::Model*)NULL, "model_ship1" );
-			break;
-	}
-	model->setCenter ( position.x - 15, position.y + 1, position.z - 10 );
-	if ( abs ( speed.x ) > abs ( speed.z ) )
-	{
-		if ( speed.x < 0 )
-			model->setAngle ( PI / 2, PI, 0 );
-		else
-			model->setAngle ( PI / 2, 0, 0 );
-	}
-	else
-	{
-		if ( speed.z < 0 )
-			model->setAngle ( PI / 2, PI / 2, 0 );
-		else
-			model->setAngle ( PI / 2, 3*PI / 2, 0 );
-	}
-
-
-
-	IGC::Texture* texture = factory->acquire( (IGC::Texture*)NULL, "back_screen_menu" );
-	texture->bind();
+	IGC::Texture* texture = factory->acquire ( (IGC::Texture*)NULL, "logo_orange" );
+	texture->bind ( );
 	renderer->setTransparency( false );
-	model->render();*/
+
+	vector<CartesianVector>::const_iterator it1, it2;
+
+	for (it1 = vertexes.begin(), it2 = it1 + 1; it2 != vertexes.end(); it1 = it2, ++it2)
+	{
+		if (fabs(it1->x - it2->x) > fabs(it1->z - it2->z))
+		{
+			model = factory->acquire ( (IGC::Model*)NULL, "model_wallX" );
+			model->grow( fabs(it1->x - it2->x), 1.0f, 1.0f );
+		}
+		else
+		{
+			model = factory->acquire ( (IGC::Model*)NULL, "model_wallZ" );
+			model->grow( 1.0f, 1.0f, fabs(it1->z - it2->z) );	
+		}
+		model->setCenter ( fabs(it1->x + it2->x)/2 - 10, 0.5f, fabs(it1->z + it2->z)/2 - 15);
+		model->render();
+	}
 }
 
-bool Wall::IsInCollision(const Rectangle& object) const
+bool Wall::IsInCollision(const Utils::Rectangle& object) const
 {
 	vector<CartesianVector>::const_iterator it1, it2;
 
